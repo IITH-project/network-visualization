2aeb559a200e599f570008614aa4b0dd003918eb
kumarsuraj151 <102572879+kumarsuraj151@users.noreply.github.com>
2023-08-25 17:21:35 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import data from './data.json'
 import Graph from './Graph';
 
 function App() {
-  console.log(data.nodes)
+  // console.log(data.nodes)
   return (
     <div className="App">
         <Graph/>
 
-        <a class="github-button" href="https://github.com/your-username" target="_blank">
+        <a class="github-button" href="https://github.com/IITH-project/network-visualization#internet-topology-visualization-using-react-and-react-vis-graph" target="_blank">
         <span class="icon"></span>
         GitHub
     </a>
